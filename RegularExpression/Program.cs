708ede763b28628ddd12ab467f572db04884b5d9
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-26 00:29:57 +0530

    [jayaprakash] setting password of using minimum 8 characters

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 
 Console.WriteLine("welcome to the regular expression program");
-Console.WriteLine("1,checking userfirstname\n2.checking userLatname\n3.checking useremailId\n4.checking userPhoneNumber");
+Console.WriteLine("1,checking userfirstname\n2.checking userLatname\n3.checking useremailId\n4.checking userPhoneNumber\n5.checking userPassword");
 Console.WriteLine("choose what program u want");
 int choose = Convert.ToInt32(Console.ReadLine());
 switch (choose)
@@ -20,12 +20,12 @@ switch (choose)
             }
             else
             {
-                Console.WriteLine("{0} invalid of user firstname",input);
+                Console.WriteLine("{0} invalid of user firstname", input);
             }
 
         }
         break;
-        case 2:
+    case 2:
         Console.WriteLine("checking Last name of user ");
         string p = @"^[A-Z][a-zA-Z]{2,}$";
         string[] LastName = { "Mudigonda", "mudigonda", "Ponnaganti", "ponnaganti" };
@@ -44,7 +44,7 @@ switch (choose)
 
         }
         break;
-        case 3:
+    case 3:
         Console.WriteLine("checking valid email");
         string mail = @"^[a-zA-Z0-9]([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z]{2,}){1,2}$";
         string[] email = { "jayaprakashjp187@gmail.com", "jp.88@gmail.com", "jp@gmail.com", "jp.com" };
@@ -63,10 +63,10 @@ switch (choose)
 
         }
         break;
-        case 4:
+    case 4:
         Console.WriteLine("checking Last name of user ");
         string phoneNumber = @"^91[ ][0-9]{10}$";
-        string[] phone = {"91 9640935321","91 7337545935","916361419518","91 9398039076","916303424324"};
+        string[] phone = { "91 9640935321", "91 7337545935", "916361419518", "91 9398039076", "916303424324" };
         foreach (string input in phone)
         {
             if (Regex.IsMatch(input, phoneNumber))
@@ -82,5 +82,25 @@ switch (choose)
 
         }
         break;
+    case 5:
+        Console.WriteLine("checking password");
+        string[] password = { "jayaprakash", "Jayaprakash", "jayp" };
+        string pass = "^[a-zA-Z]{8,}$";
+        foreach (string input in password)
+        {
+            if (Regex.IsMatch(input, pass))
 
+
+            {
+                Console.WriteLine("{0} is a valid  of user password", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} invalid of user password", input);
+            }
+
+
+
+        }
+        break;
 }
