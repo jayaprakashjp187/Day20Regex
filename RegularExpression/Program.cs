21037a4afd496977a045ded2c5140a47e853f806
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-26 16:46:10 +0530

    [jayaprakash] setting a password by using exact one special character and password should be minimum 8

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 
 Console.WriteLine("welcome to the regular expression program");
-Console.WriteLine("1,checking userfirstname\n2.checking userLatname\n3.checking useremailId\n4.checking userPhoneNumber\n5.checking userPassword\n6. checking user Password withAtleast one capital letter\n7. checking user Password withAtleast one Number");
+Console.WriteLine("1,checking userfirstname\n2.checking userLatname\n3.checking useremailId\n4.checking userPhoneNumber\n5.checking userPassword\n6. checking user Password withAtleast one capital letter\n7. checking user Password withAtleast one Number\n8. checking user Password withExact one special character");
 Console.WriteLine("choose what program u want");
 int choose = Convert.ToInt32(Console.ReadLine());
 switch (choose)
@@ -106,9 +106,9 @@ switch (choose)
         case 6:
         Console.WriteLine("setting a password with atleast one capital");
 
-        string[] passWord = { "jayaprakash", "Jayaprakash", "jayp","Jaya","JAYAPRAk" };
+        string[] passWord = { "jayaprakash", "JayaPrakash", "jayp","Jaya","JAYAPRAk","Jayapra@" };
      
-        string passInput = "^(?=.*[A-Z])([a-z]{0,}).{8,}$";
+        string passInput = @"^(?=.*[A-Z])[A-Za-z]{8,}$";
 
         foreach (string input in passWord)
         {
@@ -129,8 +129,8 @@ switch (choose)
         break;
         case 7:
         Console.WriteLine("setting a password with atleast one number");
-        string a = "^(?=.*[A-Z])([a-z]{0,})(?=.*[0-9]).{8,}$";
-        string[] passwordWithNum = { "Jaya1prakash", "jayaprakash", "Jayaprakash", "Jayaprakash1524" };
+        string a = @"^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";
+        string[] passwordWithNum = { "Jaya1prakash#", "jayaprakash", "Jayaprakash", "Jayaprakash1524" };
         foreach (string input in passwordWithNum)
         {
             if (Regex.IsMatch(input, a))
@@ -147,6 +147,25 @@ switch (choose)
 
 
         }
-        break;
 
+        break;
+        case 8:
+        Console.WriteLine("setting a password with exact one special character provided");
+        string b = "^(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*[@#$%^&+=!].*[@#$%^&+=!])[A-Za-z0-9@#$%^&+=!]{8,}$";
+        string[] passwordWithExactOneSpecialChar = { "Jaya1praka@sh", "jayaprakash", "Jayaprakash", "Jayaprakash1524", "Jayaprakash143@#"};
+        foreach (string input in passwordWithExactOneSpecialChar)
+        {
+            if (Regex.IsMatch(input, b))
+
+
+            {
+                Console.WriteLine("{0} is a valid  of user password", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} invalid of user password", input);
+            }
+            
+        }
+        break;
 }
